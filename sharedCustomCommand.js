ca67b60ad6b04051e1f4086ce6b1574db5d27342
David Lukic <david.lukic-hanomihl@qover.com>
2024-05-19 21:19:11 +0200

    added version 1.0.1

--- sharedCustomCommand.js
+++ sharedCustomCommand.js
@@ -1,5 +1,5 @@
-// cypress-shared/commands.js
-Cypress.Commands.add('printVersion', () => {
-    cy.log('Version !')
+export const COMMANDS_VERSION = '1.0.1';
+
+Cypress.Commands.add('printCommandVersion', () => {
+  console.log(`${COMMANDS_VERSION}`);
 });
-  
